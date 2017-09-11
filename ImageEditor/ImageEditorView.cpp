6348f25ed971687ed89b8e6bd411f28cf8a4e2ae
Dailin Hu <213160842@seu.edu.cn>
2017-09-11 17:15:26 +0800

    double-buffer(malfunctioning: unable to draw connected lines)

--- ImageEditor/ImageEditorView.cpp
+++ ImageEditor/ImageEditorView.cpp
@@ -43,8 +43,6 @@ CImageEditorView::CImageEditorView()
 	LBtnDn = false;
 	ImgHeight = 500;
 	ImgWidth = 500;
-	
-//	m_Current_LBtnDn_Position_Last(0,0);
 	// TODO: add construction code here
 }
 
@@ -67,20 +65,29 @@ void CImageEditorView::OnDraw(CDC *pDC)
 {
 	CImageEditorDoc *pDoc = GetDocument();
 	ASSERT_VALID(pDoc);
+	CRect rc_size;	
+	GetClientRect(&rc_size);
+	
 	///create new bitmap//////
-	// CBitmap bmp;
 	if (createNewFile.ResetMap == true){
-		CRect rc_size;	
-		GetClientRect(&rc_size);		
-		if(createNewFile.m_New_Img_Horizontal && createNewFile.m_New_Img_Vertical){
-			ImgHeight = (atoi(createNewFile.m_New_Img_Horizontal) < rc_size.right)?atoi(createNewFile.m_New_Img_Horizontal) : rc_size.right;
-			ImgWidth = (atoi(createNewFile.m_New_Img_Vertical) < rc_size.bottom)?atoi(createNewFile.m_New_Img_Vertical) : rc_size.bottom;	
-		}
+		ImgHeight = (atoi(createNewFile.m_New_Img_Horizontal) < rc_size.right)?atoi(createNewFile.m_New_Img_Horizontal) : rc_size.right;
+		ImgWidth = (atoi(createNewFile.m_New_Img_Vertical) < rc_size.bottom)?atoi(createNewFile.m_New_Img_Vertical) : rc_size.bottom;	
 		pDC->FillSolidRect(0,0,rc_size.right,rc_size.bottom,RGB(128,128,128));
 		pDC->FillSolidRect(0,0,ImgWidth,ImgHeight,RGB(255,255,255));
 		createNewFile.ResetMap = false;
 		}
-///////double buffer//////
+
+	///////double buffer//////
+	CDC MemDC;
+	MemDC.CreateCompatibleDC(pDC);
+	CBitmap newBitmap, oldBitmap;
+	oldBitmap.CreateCompatibleBitmap(pDC,rc_size.Width(),rc_size.Height());
+	MemDC.SelectObject(&oldBitmap);
+
+	MemDC.FillSolidRect(0,0,rc_size.right,rc_size.bottom,RGB(128,128,128));
+	MemDC.FillSolidRect(0,0,ImgWidth,ImgHeight,RGB(255,255,255));
+
+	pDC->BitBlt(rc_size.left,rc_size.top,rc_size.Width(),rc_size.Height(),&MemDC,0,0,SRCCOPY);
 
 	///////set color//////////
 	int red = atoi(colorPanel.m_Color_Red);
